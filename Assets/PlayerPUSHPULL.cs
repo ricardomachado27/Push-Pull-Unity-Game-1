28c879e9ef8a948e0ea5ae9149f2793adb8097c3
ricardomachado27 <ricardotmachado2023@gmail.com>
2026-06-15 17:31:35 -0400

    Update PlayerPUSHPULL.cs

--- Assets/PlayerPUSHPULL.cs
+++ Assets/PlayerPUSHPULL.cs
@@ -14,23 +14,32 @@ public class PlayerPUSHPULL : MonoBehaviour
     void Update()
     {
         Physics2D.queriesStartInColliders = false;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, distance, boxMask);
 
         if (hit.collider != null && hit.collider.gameObject.tag == "pushable" && Input.GetKeyDown(KeyCode.E))
         {
+            if (box != null)
+            {
+                box.GetComponent<FixedJoint2D>().enabled = false;
+            }
+
             box = hit.collider.gameObject;
             box.GetComponent<FixedJoint2D>().enabled = true;
             box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
         }
         else if (Input.GetKeyUp(KeyCode.E))
         {
-            box.GetComponent<FixedJoint2D>().enabled = false;
-        }  // ← was missing this
-    }  // ← was missing this
+            if (box != null)
+            {
+                box.GetComponent<FixedJoint2D>().enabled = false;
+                box = null;
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * distance);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + (Vector2)transform.right * distance);
     }
 }
