930239f6bd141a49169821f086e8a8ac3aef45ce
ricardomachado27 <ricardotmachado2023@gmail.com>
2026-06-15 14:31:54 -0400

    Death Object & Level

---
+++ Assets/FinishLine.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+
+    {
+       if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+    }
+}
