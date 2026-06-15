d0d92c702a21ccd2cb2a50a1e240b1f25d8f584a
ricardomachado27 <ricardotmachado2023@gmail.com>
2026-06-14 20:00:06 -0400

    Ground Check

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,10 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private float Move;
 
+    public float jump;
+
+    public bool isJumping;
+
     private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +24,27 @@ public class PlayerMovement : MonoBehaviour
 
         rb.linearVelocity = new Vector2(speed * Move, rb.linearVelocity.y);
 
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
+        }
+
+
         
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
     }
 }
